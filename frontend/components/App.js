e2cd28f0c515b10e517bf5e88838c0fde33f6606
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-02-20 21:03:26 -0600

    Login Form almost completed.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
 import Articles from './Articles'
 import LoginForm from './LoginForm'
@@ -18,8 +19,13 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+
+  const redirectToLogin = () => { 
+    /* ✨ implement */ return navigate('/')}
+
+
+  const redirectToArticles = () => { 
+    /* ✨ implement */ return navigate('/articles')}
 
   const logout = () => {
     // ✨ implement
@@ -36,6 +42,18 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    setSpinnerOn(true);
+    setMessage('');
+    axios.post(loginUrl, { username: username, password: password} )
+    .then(res => {
+      setSpinnerOn(false);
+      setMessage('Here are your articles, Foo!');
+      localStorage.setItem('token', res.data.token);
+      redirectToArticles();
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   const getArticles = () => {
@@ -68,8 +86,8 @@ export default function App() {
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner spinnerOn={spinnerOn} />
+      <Message message={message} />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +96,21 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login}/>} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm 
+              postArticle={postArticle}
+              updateArticle={updateArticle}
+              deleteArticle={deleteArticle}
+              />
+              <Articles 
+              articles={articles}
+              getArticles={getArticles}
+              currentArticleId={currentArticleId}
+              updateArticle={updateArticle}
+              deleteArticle={deleteArticle}
+              />
             </>
           } />
         </Routes>
